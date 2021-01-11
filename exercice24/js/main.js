18bb8815e853aeda221788ef6beb81991d6c67cf
BlasDams <samassaadama@hotmail.fr>
2021-01-12 00:10:05 +0100

    exo 24 inacheve

---
+++ exercice24/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#bg").animate({left: "100px"})
+    })
+});
+//la div se deplace mais l'image (je n'ai pas encore reussi a la detecter)
