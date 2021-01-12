17797c45119ab6134713d969e564af1e6442280f
BlasDams <samassaadama@hotmail.fr>
2021-01-12 15:37:00 +0100

    exo 19 acheve

---
+++ exercice19/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("input").attr("disabled", false); //crochet
+        //$("input").prop("disabled", false); //uppercut
+        //$("input").removeAttr("disabled"); //soumission !!!
+    })
+});
