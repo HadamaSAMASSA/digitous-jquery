8fda158cb51330aae03d61c7c34e1a5c5f34dc95
BlasDams <samassaadama@hotmail.fr>
2021-01-13 16:14:58 +0100

    exo 34 val ajax html

---
+++ exercice34/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        
+        $.ajax({
+            url:"https://restcountries.eu/rest/v2/name/"+$("input").val(),
+            success: function(data, status, response){
+                $("#country").html(data[0].name)
+                $("#capital").html(data[0].capital);
+            }
+        })
+    })
+});
+//woooooooooooo !!!!!!!
